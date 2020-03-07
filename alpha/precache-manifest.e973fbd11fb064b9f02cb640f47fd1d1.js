5111dfe8131824754d4cae4cd3aa6cbf1008fb2e
rmelillo <rmelillo@udel.edu>
2020-03-06 23:17:57 -0500

    Updates

---
+++ alpha/precache-manifest.e973fbd11fb064b9f02cb640f47fd1d1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/citations.md"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/images/background.png"
+  },
+  {
+    "revision": "475001de4261a3cc9e18005973f3bae3",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/images/colors.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/images/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/images/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/images/ship3.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-rmelillo/alphaassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "f54331ba75e13072c072",
+    "url": "/coding-2-try-phaser-rmelillo/alphagen.main.ac58a861479d314c0e81.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-rmelillo/alphagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "0d1ed038c81267902cdc2ca57588a9a1",
+    "url": "/coding-2-try-phaser-rmelillo/alphaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-rmelillo/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-rmelillo/alphasw.js"
+  }
+]);
